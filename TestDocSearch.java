f52eabc82892a44a594e5648d55e9982cdeae9c1
binkybarnes <132546955+binkybarnes@users.noreply.github.com>
2024-04-30 14:46:26 -0700

    added rat.txt in technical

--- TestDocSearch.java
+++ TestDocSearch.java
@@ -10,7 +10,7 @@ public class TestDocSearch {
     public void testIndex() throws URISyntaxException, IOException {
         Handler h = new Handler("./technical/");
         URI rootPath = new URI("http://localhost/");
-        assertEquals("There are 1391 total files to search.", h.handleRequest(rootPath));
+        assertEquals("There are 1392 total files to search.", h.handleRequest(rootPath));
     }
 
     @Test
@@ -37,5 +37,23 @@ public class TestDocSearch {
                 "Found 2 paths:\n.%1$stechnical%1$s911report%1$schapter-13.2.txt\n.%1$stechnical%1$sbiomed%1$s1477-7827-1-13.txt",
                 sep);
         assertEquals(expect, h.handleRequest(rootPath));
+
     }
+
+    @Test
+    public void testSearch3() throws URISyntaxException, IOException {
+
+        // grep -l -r "RAA" technical ─╯
+        // technical/biomed/1477-7827-1-13.txt
+        // technical/911report/chapter-13.2.txt
+        Handler h = new Handler("./technical/");
+        String sep = File.separator;
+        URI rootPath = new URI("http://localhost/search?q=fatality");
+        String expect = String.format(
+                "Found 12 paths:\n.%1$stechnical%1$s911report%1$schapter-9.txt\n.%1$stechnical%1$sbiomed%1$s1468-6708-3-10.txt\n.%1$stechnical%1$sbiomed%1$s1471-2172-4-2.txt\n.%1$stechnical%1$sbiomed%1$s1471-2288-3-4.txt\n.%1$stechnical%1$sbiomed%1$s1471-2288-3-9.txt\n.%1$stechnical%1$sbiomed%1$s1471-2350-3-1.txt\n.%1$stechnical%1$sbiomed%1$s1471-2458-2-21.txt\n.%1$stechnical%1$sbiomed%1$s1471-2466-1-1.txt\n.%1$stechnical%1$sbiomed%1$s1472-6963-3-7.txt\n.%1$stechnical%1$sgovernment%1$sGen_Account_Office%1$sSept27-2002_d02966.txt\n.%1$stechnical%1$sgovernment%1$sGen_Account_Office%1$sgg96118.txt\n.%1$stechnical%1$sgovernment%1$sGen_Account_Office%1$sog97003.txt",
+                sep);
+        assertEquals(expect, h.handleRequest(rootPath));
+
+    }
+
 }
